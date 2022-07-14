fa13f6cd5fed81578a54bf914aa1a5f598f636bf
JustynGreen <32859500+JustynGreen-V@users.noreply.github.com>
2022-07-14 19:00:10 +1000

    Added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/Versent-CTO/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "azurerm_resource_group"
+  resource_group_name = "justyngreen2-workshop"
 
 }
